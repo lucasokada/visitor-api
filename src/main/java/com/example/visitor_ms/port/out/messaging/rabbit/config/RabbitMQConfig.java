3e289c59cac3770b6c0e5b8eda1dc542b2c61361
Lucas Okada <leiki@ciandt.com>
2024-10-15 03:05:19 -0300

    adiciona configuracoes de deserializacao para dados da fila

--- src/main/java/com/example/visitor_ms/port/out/messaging/rabbit/config/RabbitMQConfig.java
+++ src/main/java/com/example/visitor_ms/port/out/messaging/rabbit/config/RabbitMQConfig.java
@@ -1,20 +1,35 @@
 package com.example.visitor_ms.port.out.messaging.rabbit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
+
     private static final String QUEUE_NAME = "EVENTS_QUEUE";
     private static final String EXCHANGE_NAME = "amq.direct";
+    private static final String RABBITMQ_HOST = "localhost";
+    private static final int RABBITMQ_PORT = 5672;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(RABBITMQ_HOST, RABBITMQ_PORT);
+        connectionFactory.setUsername("root");
+        connectionFactory.setPassword("root");
+        return connectionFactory;
+    }
 
     @Bean
     Queue queue() {
@@ -43,5 +58,17 @@ public class RabbitMQConfig {
         rabbitAdmin.declareBinding(binding);
 
         return rabbitAdmin;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter(objectMapper));
+        return rabbitTemplate;
     }
 }
