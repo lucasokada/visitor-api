c15b6354dff3a3234c972041524c4e1fa7537695
M4NT <yan0507@live.com>
2024-10-16 13:08:16 -0300

    add info visitante

--- servicea/src/main/java/com/example/servicea/VisitanteModel.java
+++ servicea/src/main/java/com/example/servicea/VisitanteModel.java
@@ -2,20 +2,53 @@ package com.example.servicea;
 
 public class VisitanteModel {
     private String nome;
-    private String sobrenome;
-    private int idade;
+    private String cpf;
+    private String dataNascimento;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String telefoneResidencial;
+    private String telefoneComercial;
+    private String celular;
+    private String email;
+    private String usuario;
+    private String senha;
+    private String tipoVisitante;
+    private String empresaNome;
+    private String empresaCnpj;
 
     // Construtor padrão
     public VisitanteModel() {
     }
 
     // Construtor parametrizado
-    public VisitanteModel(String nome, String sobrenome, int idade) {
+    public VisitanteModel(String nome, String cpf, String dataNascimento, String rua, int numero, String bairro, String cidade,
+                          String estado, String cep, String telefoneResidencial, String telefoneComercial, String celular,
+                          String email, String usuario, String senha, String tipoVisitante, String empresaNome, String empresaCnpj) {
         this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.idade = idade;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefoneResidencial = telefoneResidencial;
+        this.telefoneComercial = telefoneComercial;
+        this.celular = celular;
+        this.email = email;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.tipoVisitante = tipoVisitante;
+        this.empresaNome = empresaNome;
+        this.empresaCnpj = empresaCnpj;
     }
 
+    // Getters e Setters para todos os campos
     public String getNome() {
         return nome;
     }
@@ -24,19 +57,139 @@ public class VisitanteModel {
         this.nome = nome;
     }
 
-    public String getSobrenome() {
-        return sobrenome;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
-    public int getIdade() {
-        return idade;
+    public String getDataNascimento() {
+        return dataNascimento;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefoneResidencial() {
+        return telefoneResidencial;
+    }
+
+    public void setTelefoneResidencial(String telefoneResidencial) {
+        this.telefoneResidencial = telefoneResidencial;
+    }
+
+    public String getTelefoneComercial() {
+        return telefoneComercial;
+    }
+
+    public void setTelefoneComercial(String telefoneComercial) {
+        this.telefoneComercial = telefoneComercial;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipoVisitante() {
+        return tipoVisitante;
+    }
+
+    public void setTipoVisitante(String tipoVisitante) {
+        this.tipoVisitante = tipoVisitante;
+    }
+
+    public String getEmpresaNome() {
+        return empresaNome;
+    }
+
+    public void setEmpresaNome(String empresaNome) {
+        this.empresaNome = empresaNome;
+    }
+
+    public String getEmpresaCnpj() {
+        return empresaCnpj;
+    }
+
+    public void setEmpresaCnpj(String empresaCnpj) {
+        this.empresaCnpj = empresaCnpj;
     }
 }
